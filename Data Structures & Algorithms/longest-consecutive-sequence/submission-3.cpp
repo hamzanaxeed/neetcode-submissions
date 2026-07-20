c6c9e43959fd9377268e19bf720ba45f456bed67
Hamza Naveed <hamza.naxeed@gmail.com>
2026-07-20 15:03:06 +0500

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        
+        set<int>my_Set(nums.begin(),nums.end());
+        
+        int max_Length=0;
+        int last_Digit=-9999;
+        int length=0;
+
+        for(auto it:my_Set){
+
+            if(last_Digit==-9999 ||it==last_Digit+1){
+                length++;
+          
+            }else{
+                length=1;  
+            }
+            last_Digit=it;
+
+            if(length>max_Length){
+                max_Length=length;
+            }
+        
+            
+        }
+        return max_Length;
+    }
+};
