3cc058fb06a5fdc4b348eabb13c5f109ca3df52b
Hamza Naveed <hamza.naxeed@gmail.com>
2026-07-24 21:55:29 +0500

    Add: minimum-window-with-characters - submission-0

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+
+    bool is_Same(unordered_map<char,int>& first,unordered_map<char,int>& second){
+
+        for (auto elements:first)
+            if(elements.second>second[elements.first])return false;
+        
+        return true;
+    }
+
+    string minWindow(string s, string t) {
+    
+        if (t.size()==0)return "";
+        if (t.size()>s.size()) return "";
+
+        unordered_map<char,int> my_Map_T;
+
+        for(auto n:t){
+            my_Map_T[n]++;
+        }
+
+        int left=0;
+        int right=0;
+        int min_Length=INT_MAX;
+
+        int best_Start=0;
+        int best_End=0;
+
+        const int string_Size=s.size();
+        unordered_map<char,int> second_Map;
+
+        while(right<string_Size){
+            
+            second_Map[s[right++]]++;
+            while(is_Same(my_Map_T,second_Map)){
+                
+                if (min_Length>(right-left)){
+                    min_Length=(right-left);
+                    best_Start=left;
+                    best_End=right;
+                }
+                second_Map[s[left]]--;
+                left++;
+            }
+        }
+
+        string ans="";
+        for(int i=best_Start;i<best_End;i++){
+            ans+=s[i];
+        }
+
+        return ans;
+    }
+};
