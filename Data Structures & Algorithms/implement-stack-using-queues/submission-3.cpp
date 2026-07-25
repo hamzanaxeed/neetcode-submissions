8f7cb31e4df30addfa04983ae0f76f7de4e2346b
Hamza Naveed <hamza.naxeed@gmail.com>
2026-07-25 21:17:27 +0500

    Add: implement-stack-using-queues - submission-3

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-3.cpp
@@ -0,0 +1,43 @@
+class MyStack {
+
+queue<int> queue1;
+queue<int> queue2;
+
+public:
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        queue2.push(x);
+
+        while(!queue1.empty()){
+            queue2.push(queue1.front());
+            queue1.pop();
+        }
+        swap(queue1,queue2);
+    }
+    
+    int pop() {
+        int num=queue1.front();
+        queue1.pop();
+        return num;
+    }
+    
+    int top() {
+        return queue1.front();
+    }
+    
+    bool empty() {
+        return queue1.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
