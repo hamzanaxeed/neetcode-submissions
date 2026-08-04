e6847163d9ef1b05886c23a0089b507088d86135
Hamza Naveed <hamza.naxeed@gmail.com>
2026-08-04 11:03:15 +0500

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        
+        const int k=temperatures.size();
+        if(k==0) return {};
+
+        stack<pair<int,int>> monotonic_Stack;
+        vector<int> results(k,0);
+        
+
+        for(int i=k-1;i>=0;i--){
+
+            while (!monotonic_Stack.empty() &&
+                monotonic_Stack.top().first <= temperatures[i]) {
+                monotonic_Stack.pop();
+            }
+
+            if (!monotonic_Stack.empty())
+                results[i] = monotonic_Stack.top().second - i;
+
+            monotonic_Stack.push({temperatures[i], i});
+                    
+        }
+        
+        return results;
+    }
+};
