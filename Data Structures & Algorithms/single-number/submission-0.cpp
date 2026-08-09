d1df1ada37c0813b95ad78c7b0837312985a1f54
Hamza Naveed <hamza.naxeed@gmail.com>
2026-08-09 11:24:00 +0500

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans=0;
+
+        for(int &nu:nums){
+            ans^=nu;
+        }
+        return ans;
+    }
+};
